1c5e4fdbe49e36fae3bfaee74ffc47e9fb4b74d1
federicoBetti <federico2.betti@mail.polimi.it>
2017-11-30 00:12:37 +0100

    new migration built

---
+++ Travlendar/db/migrate/20171129230531_add_foreign_keys_to_meeting_travels_locations.rb
@@ -0,0 +1,25 @@
+class AddForeignKeysToMeetingTravelsLocations < ActiveRecord::Migration[5.1]
+  def change
+    add_column :default_locations, :user_id, :integer
+    add_foreign_key :default_locations, :users
+    add_column :default_locations, :location_id, :integer
+    add_foreign_key :default_locations, :locations
+
+    add_column :constraints, :user_id, :integer
+    add_foreign_key :constraints, :users
+
+    add_column :meeting_participations, :user_id, :integer
+    add_foreign_key :meeting_participations, :users
+    add_column :meeting_participations, :meeting_id, :integer
+    add_foreign_key :meeting_participations, :meetings
+
+    add_column :travels, :meeting_participation_id, :integer
+    add_foreign_key :travels, :meeting_participations
+
+    add_column :travel_steps, :travel_id, :integer
+    add_foreign_key :travel_steps, :travels
+
+    add_column :meetings, :location_id, :integer
+    add_foreign_key :meetings, :locations
+  end
+end
