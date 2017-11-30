510220f2d8e2bcedc9f23acfa8cee4ea3d367cbd
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-11-30 21:58:53 +0100

    Fixed supercategory foreign key

--- Travlendar/app/models/category.rb
+++ Travlendar/app/models/category.rb
@@ -1,6 +1,4 @@
 class Category < ApplicationRecord
     has_many :subclasses, class_name: "Category", foreign_key: "superclass_id"
-    belongs_to :superclass, class_name: "Category"
-
-    validates :superclass, presence: false
+    belongs_to :superclass, class_name: "Category", optional: true
 end
