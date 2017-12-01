b5b925094accbfb5fcf2f6968aabc643bc1d5986
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-01 12:09:32 +0100

    modified meeting participation model

--- Travlendar/app/models/meeting_participation.rb
+++ Travlendar/app/models/meeting_participation.rb
@@ -2,6 +2,6 @@ class MeetingParticipation < ApplicationRecord
   belongs_to :user
   belongs_to :meeting
 
-  belongs_to :arriving_travel, class_name: 'travel', foreign_key: 'arriving_travel_id'
-  belongs_to :leaving_travel, class_name: 'travel', foreign_key: 'leaving_travel_id'
+  belongs_to :arriving_travel, class_name: 'Travel', foreign_key: 'arriving_travel_id'
+  belongs_to :leaving_travel, class_name: 'Travel', foreign_key: 'leaving_travel_id'
 end
