8512f4645bbdc7f258b8cbac5f0135161657ca4a
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-02 16:43:24 +0100

    added new routes

--- Travlendar/config/routes.rb
+++ Travlendar/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "homepage#index", :as => 'root'
+  post "/" => "homepage#post_index"
+
+  resources :user
+  resources :homepage
 end
