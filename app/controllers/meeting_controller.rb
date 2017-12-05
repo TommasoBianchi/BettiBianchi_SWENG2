111f4e3eb5c53beeff1da6b19dcdccffab220c48
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-05 19:31:48 +0100

    working on meeting page

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,3 +1,7 @@
 class MeetingController < ApplicationController
-  def show; end
+  def show
+    @user = current_user
+    @meeting = Meeting.find(1)
+    @meeting.abstract = 'prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova '
+  end
 end
