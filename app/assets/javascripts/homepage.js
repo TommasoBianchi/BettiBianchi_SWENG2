92bdf7c98a1cf2b7467d38500c61d1aff9cce6c0
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-05 18:45:07 +0100

    Removed homepage.js

--- app/assets/javascripts/homepage.js
+++
@@ -1,9 +0,0 @@
-$('.input').emailError({
-  document.getElementById('emailField').value('Wrong Email')
-});
-$('.input').passwordError({
-  document.getElementById('emailField').value('Wrong Email')
-});
-$('.input').inputError({
-  document.getElementById('emailField').value('Wrong Email')
-});
