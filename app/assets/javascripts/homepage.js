1a1c8f42e12153360a3461f96e65e8d007b308fa
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-05 18:39:44 +0100

    Removed useless code from homepage.js

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,9 +1,1 @@
-$('.input').emailError({
-  document.getElementById('emailField').value('Wrong Email')
-});
-$('.input').passwordError({
-  document.getElementById('emailField').value('Wrong Email')
-});
-$('.input').inputError({
-  document.getElementById('emailField').value('Wrong Email')
-});
+
