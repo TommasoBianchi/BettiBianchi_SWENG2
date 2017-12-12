5616061c4770da62bf412aca2cd7645504411a44
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-12 20:20:31 +0100

    nothing changed, isEmail method to review!

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -108,7 +108,7 @@ class HomepageController < ApplicationController
     end
   end
 
-  def isEmail(str)
+  def isEmail(_str)
     str.match(/[a-zA-Z0-9._%]@(?:[a-zA-Z0-9]\.)[a-zA-Z]{2,4}/)
   end
 end
