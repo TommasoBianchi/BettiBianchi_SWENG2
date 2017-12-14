4ac47d30df2c55c78cde2dfae0ab01eb67701106
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-14 16:42:50 +0100

    fixed login with email

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,114 +1,116 @@
 class HomepageController < ApplicationController
-  Error_message = 'Invalid email/password combination'.freeze
+	Error_message = 'Invalid email/password combination'.freeze
 
-  skip_before_action :require_login
+	skip_before_action :require_login
 
-  def index
-    @user = User.new
-  end
+	def index
+		@user = User.new
+	end
 
-  def post_index
-    return unless validate_input
+	def post_index
+		return unless validate_input
 
-    email = Email.find_by(email: params[:homepage][:email].downcase)
-    user = User.find_by(primary_email_id: email.id) if email
+		email = Email.find_by(email: params[:homepage][:email].downcase)
+		user = User.find_by(primary_email_id: email.id) if email
 
-    if params[:signup] # button clicked = signup
-      if email
-        user_existing_email(user)
-      else # email doesn't exist
-        if isEmail(params[:homepage][:email].downcase)
-          signup_incomplete_user
-        else # input is not an email
-          nickname_user = User.find_by(nickname: params[:homepage][:email])
-          if nickname_user # input corresponds to a valid nickname
-            user_existing_email(nickname_user)
-          else # guest tries to signup with a non-valid email
-            flash.now[:danger] = 'You must register with a valid email'
-            render 'index'
-          end # end if nickname_user
-        end # end isEmail
-      end # end if email
-    else # button clicked = login
-      nickname_user = User.find_by(nickname: params[:homepage][:email])
-      if email && user
-        user_existing_email(user)
-      elsif nickname_user
-        user_existing_email(nickname_user)
-      else # user doesn't exist
-        unless login_incomplete_user # neither an incomplete user exists
-          flash.now[:danger] = Error_message
-          render 'index'
-        end
-      end
-    end # end button clicked
-  end
+		if params[:signup] # button clicked = signup
+			if email
+				user_existing_email(user)
+			else # email doesn't exist
+				if isEmail(params[:homepage][:email].downcase)
+					signup_incomplete_user
+				else # input is not an email
+					nickname_user = User.find_by(nickname: params[:homepage][:email])
+					if nickname_user # input corresponds to a valid nickname
+						user_existing_email(nickname_user)
+					else # guest tries to signup with a non-valid email
+						flash.now[:danger] = 'You must register with a valid email'
+						render 'index'
+					end # end if nickname_user
+				end # end isEmail
+			end # end if email
+		else # button clicked = login
+			nickname_user = User.find_by(nickname: params[:homepage][:email])
+			if email && user
+				user_existing_email(user)
+			elsif nickname_user
+				user_existing_email(nickname_user)
+			else # user doesn't exist
+				unless login_incomplete_user # neither an incomplete user exists
+					flash.now[:danger] = Error_message
+					render 'index'
+				end
+			end
+		end # end button clicked
+	end
 
-  def login; end
+	def login;
+	end
 
-  def destroy
-    log_out
-    redirect_to homepage_path
-  end
+	def destroy
+		log_out
+		redirect_to homepage_path
+	end
 
-  private
+	private
 
-  def go_to_complete_regitration(incomplete_user)
-    puts(incomplete_user)
-    session[:tmp_checked] = incomplete_user.id
-    redirect_to '/user/new'
-  end
+	def go_to_complete_regitration(incomplete_user)
+		puts(incomplete_user)
+		session[:tmp_checked] = incomplete_user.id
+		redirect_to '/user/new'
+	end
 
-  def validate_input
-    if params['homepage']['email'].present? && params['homepage']['password'].present?
-      true
-    else
-      flash.now[:danger] = 'Some fields are not filled properly'
-      render 'index'
-      false
-    end
-  end
+	def validate_input
+		if params['homepage']['email'].present? && params['homepage']['password'].present?
+			true
+		else
+			flash.now[:danger] = 'Some fields are not filled properly'
+			render 'index'
+			false
+		end
+	end
 
-  def login_incomplete_user
-    incomplete_user = IncompleteUser.find_by(email: params[:homepage][:email].downcase)
-    if incomplete_user && incomplete_user.authenticate(params[:homepage][:password])
-      go_to_complete_regitration(incomplete_user)
-      true
-    elsif incomplete_user # incorrect password
-      flash.now[:danger] = Error_message
-      render 'index'
-      true
-    end
-  end
+	def login_incomplete_user
+		incomplete_user = IncompleteUser.find_by(email: params[:homepage][:email].downcase)
+		if incomplete_user && incomplete_user.authenticate(params[:homepage][:password])
+			go_to_complete_regitration(incomplete_user)
+			true
+		elsif incomplete_user # incorrect password
+			flash.now[:danger] = Error_message
+			render 'index'
+			true
+		end
+	end
 
-  def signup_incomplete_user
-    incomplete_user = IncompleteUser.find_by(email: params[:homepage][:email].downcase)
-    if incomplete_user && incomplete_user.authenticate(params[:homepage][:password])
-      go_to_complete_regitration(incomplete_user)
-    elsif incomplete_user # incorrect password
-      flash.now[:danger] = Error_message
-      render 'index'
-    else
-      incomplete_user = IncompleteUser.create(email: params[:homepage][:email].downcase, password: params[:homepage][:password], password_confirmation: params[:homepage][:password])
-      go_to_complete_regitration(incomplete_user)
-    end
-  end
+	def signup_incomplete_user
+		incomplete_user = IncompleteUser.find_by(email: params[:homepage][:email].downcase)
+		if incomplete_user && incomplete_user.authenticate(params[:homepage][:password])
+			go_to_complete_regitration(incomplete_user)
+		elsif incomplete_user # incorrect password
+			flash.now[:danger] = Error_message
+			render 'index'
+		else
+			incomplete_user = IncompleteUser.create(email: params[:homepage][:email].downcase, password: params[:homepage][:password], password_confirmation: params[:homepage][:password])
+			go_to_complete_regitration(incomplete_user)
+		end
+	end
 
-  def create_incomplete_user
-    incomplete_user = IncompleteUser.create(email: params[:homepage][:email].downcase, password: params[:homepage][:password], password_confirmation: params[:homepage][:password])
-  end
+	def create_incomplete_user
+		incomplete_user = IncompleteUser.create(email: params[:homepage][:email].downcase, password: params[:homepage][:password], password_confirmation: params[:homepage][:password])
+	end
 
-  def user_existing_email(user)
-    if user && user.authenticate(params[:homepage][:password]) # normal registered user
-      log_in user
-      redirect_to calendar_day_path DateTime.now.year, DateTime.now.month, DateTime.now.day
-    else # registered user that provides a wrong password
-      flash.now[:danger] = Error_message
-      render 'index'
-    end
-  end
+	def user_existing_email(user)
+		if user && user.authenticate(params[:homepage][:password]) # normal registered user
+			log_in user
+			redirect_to calendar_day_path DateTime.now.year, DateTime.now.month, DateTime.now.day
+		else # registered user that provides a wrong password
+			flash.now[:danger] = Error_message
+			render 'index'
+		end
+	end
 
-  def isEmail(str)
-    str.match(/[a-zA-Z0-9._%]@(?:[a-zA-Z0-9]\.)[a-zA-Z]{2,4}/)
-  end
+	def isEmail(str)
+		reg_ex_pattern = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+		reg_ex_pattern.match?(str)
+	end
 end
