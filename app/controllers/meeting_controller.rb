4df181c1248b217e183501526e75dda111389e68
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-14 14:03:15 +0100

    some files reformatted correctly

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,50 +1,50 @@
 class MeetingController < ApplicationController
 
-  before_action :check_participation, except: [:new]
+	before_action :check_participation, except: [:new]
 
-  def show
-    @user = current_user
-    @meeting = Meeting.find(params['id'])
-    @meeting.abstract = 'prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova '
-  end
+	def show
+		@user = current_user
+		@meeting = Meeting.find(params['id'])
+		@meeting.abstract = 'prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova prova '
+	end
 
-  def participants_page
-    @user = current_user
-    @meeting = Meeting.find(params['id'])
-  end
+	def participants_page
+		@user = current_user
+		@meeting = Meeting.find(params['id'])
+	end
 
-  def remove_from_meeting
-    m = MeetingParticipation.find_by(meeting_id: params[:meeting_id], user_id: params[:user_id])
-    m.delete
-    redirect_to participants_page_path(id: params[:meeting_id])
-  end
+	def remove_from_meeting
+		m = MeetingParticipation.find_by(meeting_id: params[:meeting_id], user_id: params[:user_id])
+		m.delete
+		redirect_to participants_page_path(id: params[:meeting_id])
+	end
 
-  def nominate_admin
-    m = MeetingParticipation.find_by(meeting_id: params[:meeting_id], user_id: params[:user_id])
-    m.is_admin = true
-    m.save
-    redirect_to participants_page_path(id: params[:meeting_id])
-  end
+	def nominate_admin
+		m = MeetingParticipation.find_by(meeting_id: params[:meeting_id], user_id: params[:user_id])
+		m.is_admin = true
+		m.save
+		redirect_to participants_page_path(id: params[:meeting_id])
+	end
 
-  def new
-    @meeting = Meeting.new
-    @users = User.all
-    @user_selected = ''
-    @user_names = %w[a b]
-  end
+	def new
+		@meeting = Meeting.new
+		@users = User.all
+		@user_selected = ''
+		@user_names = %w[a b]
+	end
 
-  def create
-    puts '****************************************'
-    puts(params)
-    puts '****************************************'
-  end
+	def create
+		puts '****************************************'
+		puts(params)
+		puts '****************************************'
+	end
 
-  private
-  def check_participation
-    meeting_id = params['id']
-    meeting_id = params['meeting_id'] unless meeting_id
-    unless current_user.meeting_participations.where(meeting_id: meeting_id).count() > 0
-      raise ActionController::RoutingError, 'Not Found'
-    end
-  end
+	private
+	def check_participation
+		meeting_id = params['id']
+		meeting_id = params['meeting_id'] unless meeting_id
+		unless current_user.meeting_participations.where(meeting_id: meeting_id).count() > 0
+			raise ActionController::RoutingError, 'Not Found'
+		end
+	end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,88 +1,81 @@
 class UserController < ApplicationController
-  skip_before_action :require_login
+	skip_before_action :require_login
 
-  def show
-    require_login
-    @user = current_user
-  end
+	def show
+		require_login
+		@user = current_user
+	end
 
-  def create
-    # problems on create user before email and viceversa
+	def create
+		# problems on create user before email and viceversa
 
-    email_hash = params.slice(:user)['user']['email']
-    params[:user].delete('email')
+		email_hash = params.slice(:user)['user']['email']
+		params[:user].delete('email')
 
-    incomplete_user = IncompleteUser.find(session[:tmp_checked])
-    incomplete_user_email = incomplete_user.email
-    incomplete_user_psw = incomplete_user.password
+		incomplete_user = IncompleteUser.find(session[:tmp_checked])
+		incomplete_user_email = incomplete_user.email
+		incomplete_user_psw = incomplete_user.password
 
-    @user = User.new(user_params)
-    unless email_hash == incomplete_user_email && incomplete_user.authenticate(@user.password)
-      flash.now[:danger] = 'Wrong mail or password'
-      @unregisterdUser = incomplete_user
-      render 'new'
-      return
-    end
+		@user = User.new(user_params)
+		unless email_hash == incomplete_user_email && incomplete_user.authenticate(@user.password)
+			flash.now[:danger] = 'Wrong mail or password'
+			@unregisterdUser = incomplete_user
+			render 'new'
+			return
+		end
 
-    if @user.valid?
-      session[:tmp_checked] = nil # delete temp variable
-      IncompleteUser.delete(incomplete_user.id)
-      email = Email.create(email: email_hash)
-      @user.emails.push(email)
-      @user.primary_email_id = email.id
-      last_user = User.last
-      email.user_id = User.last.id + 1
-      @user.save
-      log_in @user
-      redirect_to calendar_day_path DateTime.now.year, DateTime.now.month, DateTime.now.day
-    else
-      @unregisterdUser = incomplete_user
-      render 'new'
-    end
-  end
+		if @user.valid?
+			session[:tmp_checked] = nil # delete temp variable
+			IncompleteUser.delete(incomplete_user.id)
+			email = Email.create(email: email_hash)
+			@user.emails.push(email)
+			@user.primary_email_id = email.id
+			last_user = User.last
+			email.user_id = User.last.id + 1
+			@user.save
+			log_in @user
+			redirect_to calendar_day_path DateTime.now.year, DateTime.now.month, DateTime.now.day
+		else
+			@unregisterdUser = incomplete_user
+			render 'new'
+		end
+	end
 
-  def new
-    @unregisterdUser = IncompleteUser.find(session[:tmp_checked])
-    @user = User.new
-    @user.preference_list = '0123'
- end
+	def new
+		@unregisterdUser = IncompleteUser.find(session[:tmp_checked])
+		@user = User.new
+		@user.preference_list = '0123'
+	end
 
-  def index
-    respond_to do |format|
-      format.html { html_index }
-      format.json { json_index }
-    end
-  end
+	def index
+		respond_to do |format|
+			format.html {html_index}
+			format.json {json_index}
+		end
+	end
 
-  def search
-    @users = if params[:term]
-               User.search_by_full_name(params[:term])
-             else
-               User.all
-         end
-  end
 
-  private
+	private
 
-  def html_index
-    @users = User.all
-  end
+	def html_index
+		@users = User.all
+	end
 
-  def json_index
-    query = begin
-           params.permit(:query).fetch(:query)
-         rescue
-           ''
-         end
-    users = User.where('LOWER(nickname) LIKE LOWER(:query)', query: "%#{query}%")
-    render json: users.map(&:name)
-  end
+	def json_index
+		query = begin
+			params.permit(:query).fetch(:query)
+		rescue
+			''
+		end
+		users = User.where('LOWER(nickname) LIKE LOWER(:query)', query: "%#{query}%")
+		render json: users.map(&:name)
+	end
 
-  def user_params
-    params.require(:user).permit(:name, :surname, :password, :nickname, :preference_list)
-  end
+	def user_params
+		params.require(:user).permit(:name, :surname, :password, :nickname, :preference_list)
+	end
 
-  def reinsertUser(incomplete_user_email, incomplete_user_psw)
-    IncompleteUser.create(email: incomplete_user_email, password: incomplete_user_psw, password_confirmation: incomplete_user_psw)
-  end
+	def reinsertUser(incomplete_user_email, incomplete_user_psw)
+		IncompleteUser.create(email: incomplete_user_email, password: incomplete_user_psw, password_confirmation: incomplete_user_psw)
+	end
 end
