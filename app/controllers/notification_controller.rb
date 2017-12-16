86e8ee3e096f47e0b0315ff4f22422422aad23c6
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-16 16:28:09 +0100

    Fixed fetching warnings in notifications controller

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,6 +2,6 @@ class NotificationController < ApplicationController
 	def index
 		@user = current_user
     	@pending_meeting = MeetingParticipation.where('response_status = :response_status AND user_id = :user', response_status: 0, user: @user.id)
-    	@warning_meeting = MeetingParticipation.where(is_consistent: false)
+    	@warning_meeting = @user.meeting_participations.where(is_consistent: false)
 	end
 end
