61b0e2a6c88ea77fcdd584709ee6ece074480a78
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-19 16:39:25 +0100

    Modified create meeting job + update break WIP

--- app/jobs/create_meeting_job.rb
+++ app/jobs/create_meeting_job.rb
@@ -1,7 +1,11 @@
 class CreateMeetingJob < ApplicationJob
   queue_as :default
 
-  def perform(start_date, end_date, title, location, user)
-    MeetingHelper.create_meeting(Time.at(start_date), Time.at(end_date), title, location, user)
+  def perform(start_date, end_date, title, abstract, location, user, invited_users = [])
+    meeting = MeetingHelper.create_meeting(Time.at(start_date), Time.at(end_date), title, abstract, location, user)
+
+    invited_users.each do |user|
+    	InviteToMeetingJob.perform_later(meeting, user)
+    end
   end
 end
