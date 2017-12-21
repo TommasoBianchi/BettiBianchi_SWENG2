b9806a51db95edb53c18832960e1d2563eace747
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-21 19:49:55 +0100

    created page to add breaks

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -60,6 +60,19 @@ class UserController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = current_user
+		primary_mail_id = @user.primary_email_id
+		@emails = @user.emails.where.not(id: primary_mail_id)
+	end
+
+	def delete_email
+		check_delete_email_params
+		Email.delete(params[:email_id])
+		redirect_to user_path(id: params[:user_id])
+	end
+
+
 	def contacts
 		@user = current_user
 		puts ("ncjkdscnkdsjcnsdkj")
@@ -154,6 +167,66 @@ class UserController < ApplicationController
 		end
 	end
 
+	def delete_break
+		unless check_delete_break_params
+			redirect_to settings_page_path(id: params[:user_id])
+			return
+		end
+		to_delete = ComputedBreak.where(break_id: params[:break_id])
+		ComputedBreak.delete(to_delete.ids)
+		Break.delete(params[:break_id])
+		redirect_to settings_page_path(id: params[:user_id])
+	end
+
+	def add_break
+		@break = Break.new
+	end
+
+	def create_break
+		@break = Break.new
+		check_if_myself
+		unless check_date_consistency(params[:break][:start_time_slot]) && check_date_consistency(params[:break][:end_time_slot]) && check_date_consistency(params[:break][:default_time])
+			fill_time_errors
+			render 'add_break'
+			return
+		end
+
+		begin
+			starting_hour = params[:break][:start_time_slot].to_datetime.hour * 60 + params[:break][:start_time_slot].to_datetime.min
+		rescue NoMethodError
+			render 'add_break'
+			return
+		end
+
+
+		begin
+			ending_hour = params[:break][:end_time_slot].to_datetime.hour * 60 + params[:break][:end_time_slot].to_datetime.min
+		rescue NoMethodError
+			render 'add_break'
+			return
+		end
+
+		begin
+			default_time_hour = params[:break][:default_time].to_datetime.hour * 60 + params[:break][:default_time].to_datetime.min
+		rescue NoMethodError
+			render 'add_break'
+			return
+		end
+
+		unless check_if_consistent_times(starting_hour, default_time_hour, ending_hour)
+			fill_time_errors
+		end
+
+		day_of_the_week = get_day_by_name(params[:break][:day_of_the_week])
+		name = params[:break][:name]
+		duration = ending_hour - starting_hour
+		user = current_user
+		b = Break.create(default_time: default_time_hour, start_time_slot: starting_hour, end_time_slot: ending_hour,
+													duration: duration, name: name, day_of_the_week: day_of_the_week, user_id: user.id)
+		#BreakHelper.update_break(b, day?) do i have to pass all mondays(ex) from now to the eternity?
+		redirect_to settings_page_path
+	end
+
 
 	private
 
@@ -197,6 +270,7 @@ class UserController < ApplicationController
 		end
 	end
 
+	# method used to check the consistency of params used in constraint add/delate options
 	def delete_constraint_check(user, constraint)
 		unless (current_user.id == user.to_i) && (current_user.constraints.where(id: constraint).count() > 0)
 			raise ActionController::RoutingError, 'Not Found'
@@ -206,4 +280,50 @@ class UserController < ApplicationController
 	def check_constraint_params
 		params.require(:constraint).permit(:travel_mean, :subject, :operator, :value)
 	end
+
+	# method used to check the consistency of params used in break add/delate options
+	def check_delete_break_params
+		params.permit(:break_id, :user_id)
+		if params[:user_id].to_i != current_user.id
+			return false
+		end
+
+		unless Break.find(params[:break_id]).user_id == params[:user_id].to_i
+			return false
+		end
+		return true
+	end
+
+	def check_date_consistency(date_from_timepicker)
+		params_ok = true
+		begin
+			date_from_timepicker.to_datetime
+		rescue ArgumentError
+			params_ok = false
+		end
+
+		if date_from_timepicker == ""
+			params_ok = false
+		end
+		return params_ok
+	end
+
+	def fill_time_errors
+		@break.errors.add(:start_time_slot, 'inconsistent time sequence')
+		@break.errors.add(:end_time_slot, 'inconsistent time sequence')
+		@break.errors.add(:default_time, 'inconsistent time sequence')
+	end
+
+	def check_if_consistent_times(starting_hour, default_time_hour, ending_hour)
+		return (starting_hour <= default_time_hour) && (default_time_hour <= ending_hour)
+	end
+
+	# method used to check the consistency of params used in delete email
+	def check_delete_email_params
+		params.permit(:user_id, :email_id)
+		unless (current_user.id == params[:user_id].to_i) && (current_user.emails.where(id: params[:email_id]).count() > 0)
+			raise ActionController::RoutingError, 'Not Found'
+		end
+	end
+
 end
