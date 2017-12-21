e89729472d77b9fdf119ba14c763d2c886863f30
federicoBetti <federico2.betti@mail.polimi.it>
2017-12-21 19:51:12 +0100

    created a method for every controller to change days on integer0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,34 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include HomepageHelper
+	protect_from_forgery with: :exception
+	include HomepageHelper
 
-  before_action :require_login
+	before_action :require_login
 
-  private
-  def require_login
-    unless current_user
-      redirect_to homepage_path
-    end
-  end
+	def get_day_by_name(day_of_the_week)
+		case day_of_the_week
+			when "Sunday"
+				0
+			when "Monday"
+				1
+			when "Tuesday"
+				2
+			when "Wednesday"
+				3
+			when "Thursday"
+				4
+			when "Friday"
+				5
+			when "Saturday"
+				6
+			else
+				'no day'
+		end
+	end
+
+	private
+	def require_login
+		unless current_user
+			redirect_to homepage_path
+		end
+	end
 end
