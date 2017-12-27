cfd280f96071ca4cc8393a6a2a3ed6103f81f3fb
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-27 14:59:32 +0100

    Working on test data setup

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test 'login with invalid information' do
-    get root_get
-    assert_template 'index'
-    post root_post, params: { homepage: { email: 'fsd@f', password: 'fds' } }
-    assert_template 'index'
-    assert_not flash.empty?
-    get root_get
-    assert flash.empty?
-  end
+	test "db dump" do
+		puts User.all.count
+		assert true
+	end
+  # test 'login with invalid information' do
+  #  get root_get
+  #  assert_template 'index'
+  #  post root_post, params: { homepage: { email: 'fsd@f', password: 'fds' } }
+  #  assert_template 'index'
+  #  assert_not flash.empty?
+  #  get root_get
+  #  assert flash.empty?
+  #end
 end
