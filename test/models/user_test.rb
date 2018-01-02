ca1a5605ad621c020a5b20544f2377a6e2225431
federicoBetti <federico2.betti@mail.polimi.it>
2018-01-02 19:10:52 +0100

    created first tests on user and email model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,7 +14,18 @@ class UserTest < ActiveSupport::TestCase
 		end
 	end
 
-  # test "the truth" do
-  #   assert true
-  # end
+  test "unique_nickname" do
+		User.all.each do |user|
+			assert User.where(nickname: user.nickname).count == 1
+		end
+	end
+
+	test "correct_phone_number_and_pref_list" do
+		is_number = /^[0-9]+$/
+		User.all.each do |user|
+			assert is_number.match?(user.phone_number) || user.phone_number == nil
+			assert user.preference_list.length > 0
+		end
+	end
+
 end
