5fab3fb0c5111f06d724be1715cdb061b58b5384
federicoBetti <federico2.betti@mail.polimi.it>
2018-01-03 11:44:23 +0100

    working on model testing

--- test/models/travel_step_test.rb
+++ test/models/travel_step_test.rb
@@ -1,5 +1,32 @@
 require 'test_helper'
 
 class TravelStepTest < ActiveSupport::TestCase
-  # test the consistency of start time and end time
+
+	test "start and end time consistency" do
+		TravelStep.all.each do |travel_step|
+			assert travel_step.start_time <= travel_step.end_time
+		end
+	end
+
+	test "travel step start time consistency with its travel" do
+		TravelStep.all.each do |travel_step|
+			unless Travel.find(travel_step.travel_id).start_time <= travel_step.start_time
+				puts(travel_step.id)
+				puts(Travel.find(travel_step.travel_id).start_time)
+				puts(travel_step.start_time)
+			end
+			assert Travel.find(travel_step.travel_id).start_time <= travel_step.start_time
+		end
+	end
+
+	test "travel step end time consistency with its travel" do
+		TravelStep.all.each do |travel_step|
+			unless  Travel.find(travel_step.travel_id).end_time >= travel_step.end_time
+				puts(travel_step.id)
+				puts(Travel.find(travel_step.travel_id).end_time)
+				puts(travel_step.end_time)
+			end
+			assert Travel.find(travel_step.travel_id).end_time >= travel_step.end_time
+		end
+	end
 end
--- test/models/value_test.rb
+++ test/models/value_test.rb
@@ -10,9 +10,6 @@ class ValueTest < ActiveSupport::TestCase
 
 	test "no values with the same subject and value" do
 		Value.all.each do |value|
-			puts(value.subject_id)
-			puts(value.value)
-			puts(Value.where(subject: value.subject, value: value.value).count)
 			assert Value.where(subject: value.subject, value: value.value).count == 1
 		end
 	end
